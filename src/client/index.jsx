cbb26ba1cfc94edff8c06679a3232a2f2ad03541
Daryll <daryllcsh@gmail.com>
2017-05-19 14:03:36 -0700

    Give structure

---
+++ src/client/index.jsx
@@ -0,0 +1,18 @@
+import {React, Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Daryll</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,19 @@
 const path = require('path');
  
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
+  devtool: 'source-map',
+  entry: path.join(_dirname, 'src/client/index.jsx'),
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.join(__dirname, 'src/bundles'),
     filename: 'bundle.js',
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
