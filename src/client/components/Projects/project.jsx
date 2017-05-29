a71261d8f894e0b17012ab3c6b77a8d0a43392b2
Daryll <daryllcsh@gmail.com>
2017-05-28 21:46:22 -0700

    Resize stack icons

--- src/client/components/Projects/project.jsx
+++ src/client/components/Projects/project.jsx
@@ -4,17 +4,22 @@ const Project = ({ project }) => {
   return (
     <div id={ project.Name } className="section" >
       <h2>{ project.Name }</h2>
-      <p>{ project.Date }</p>
-      <p>{ project.Description }</p>
-      { project.Stack.map(stack =>
-        <div className="projectStack">{ stack }</div>
-      )}
-      { project.StackIcons.map(icon =>
-        <img className="stackIcon" src={ icon } />
-      )}
       { project.Screenshots.map(shot =>
         <img className="screenshot" src={ shot } />
       )}
+      <div className="row">
+        <div className="stackIcons">
+          { project.StackIcons.map(icon =>
+            <img className="stackIcon" src={ icon } />
+          )}
+        </div>
+        <div className="projectDescription">{ project.Description }</div>
+      </div>
+      <div className="projectStacks">
+        { project.Stack.map(stack =>
+          <div className="projectStack">{ stack }</div>
+        )}
+      </div>
       <p>{ project.Demo }</p>
       <p>{ project.Github }</p>
     </div>
