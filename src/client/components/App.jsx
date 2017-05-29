0c716afed171d5b68c993c8fbb77aa092b628457
Daryll <daryllcsh@gmail.com>
2017-05-28 19:45:05 -0700

    Add hero image

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Personal from './Personal/personal.jsx'
+import Intro from './Intro/intro.jsx'
 import Projects from './Projects/projects.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="site-container">
-        <Personal />
+        <Intro />
         <Projects />
       </div>
     );
--- src/client/components/Intro/intro.jsx
+++ src/client/components/Intro/intro.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Personal extends Component {
+class Intro extends Component {
   constructor(props) {
     super(props);
 
@@ -37,4 +37,4 @@ class Personal extends Component {
   }
 }
 
-export default Personal;
+export default Intro;
