03ecf81e25c08cf11cb8fab835ab74ae42bfeabd
Daryll <daryllcsh@gmail.com>
2017-06-03 15:01:56 -0700

    Fix script path

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/js', express.static(path.join(__dirname, '../src/js')));
+app.use('/JS', express.static(path.join(__dirname, '../src/JS')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
